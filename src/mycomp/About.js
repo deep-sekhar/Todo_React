afa61d3685e9c143aae6df66d1127556f19af58d
Deep <sekharsmemories@gmail.com>
2021-07-21 14:07:58 +0530

    learning ....

--- src/mycomp/About.js
+++ src/mycomp/About.js
@@ -4,9 +4,11 @@ export const About = () => {
     return (
         <div className="min-vh-100 container">
             <h3>
-            This is about page
+            This is About page
             </h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis modi quae dignissimos sed esse, quas deleniti fugiat, illum perspiciatis voluptatum possimus dolorem perferendis. Laboriosam vero, dolorum non eius omnis quam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio dignissimos ad tenetur at.
+            </p>
         </div>
     )
 }
